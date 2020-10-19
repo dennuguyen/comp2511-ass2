63c12c8ec137fc9e54bf86893a3f401dd638b9fd
dennuguyen <dennuguyen@gmail.com>
2020-10-20 01:51:37 +1100

    Initial commit

---
+++ src/gloriaromanus.sh
@@ -0,0 +1,6 @@
+# javac unsw/gloriaromanus/*.java -cp "../lib/*" --module-path /usr/share/openjfx/lib/ --add-modules=javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.swing,javafx.web
+
+javac unsw/gloriaromanus/*.java -cp "../lib/*"
+
+# java -cp ../lib/*: unsw.gloriaromanus.GloriaRomanusApplication "$@"
+# rm unsw/gloriaromanus/*.class
