84024d72262da8caa725cf5baadeb10be23facfb
dennuguyen <dennuguyen@gmail.com>
2020-10-23 00:46:07 +1100

    Initial commit

---
+++ src/unsw/gloriaromanus/ArtilleryMoves.java
@@ -0,0 +1,14 @@
+package unsw.gloriaromanus;
+
+public class ArtilleryMoves implements MoveType {
+
+    private int moves;
+
+    public ArtilleryMoves() {
+        this.moves = 4;
+    }
+
+    public int getMoves() {
+        return this.moves;
+    }
+}
---
+++ src/unsw/gloriaromanus/CavalryMoves.java
@@ -0,0 +1,14 @@
+package unsw.gloriaromanus;
+
+public class CavalryMoves implements MoveType {
+
+    private int moves;
+
+    public CavalryMoves() {
+        this.moves = 15;
+    }
+
+    public int getMoves() {
+        return this.moves;
+    }
+}
