af32d4fe8ea130ea856c114a78133c5f284f4125
dennuguyen <dennuguyen@gmail.com>
2020-10-25 22:41:34 +1100

    Initial commit

---
+++ src/unsw/gloriaromanus/util/Pair.java
@@ -0,0 +1,33 @@
+/**
+ * Convenient container for grouping pairs of objects
+ */
+
+package unsw.gloriaromanus.util;
+
+public class Pair<K, V> {
+
+    public final K first;
+    public final V second;
+
+    /**
+     * Pair constructor
+     * 
+     * @param first  First element of pair
+     * @param second Second element of pair
+     */
+    public Pair(K first, V second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    /**
+     * Static method for conveniently creating pairs
+     * 
+     * @param first  First element of pair
+     * @param second Second element of pair
+     * @return Created pair with first and second
+     */
+    public static <K, V> Pair<K, V> of(K first, V second) {
+        return new Pair<K, V>(first, second);
+    }
+}
