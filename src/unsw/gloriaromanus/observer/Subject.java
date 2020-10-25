19c3d89167bd7f4be6c40fa2cfc217fe338ee33f
dennuguyen <dennuguyen@gmail.com>
2020-10-25 22:31:47 +1100

    Initial commit

---
+++ src/unsw/gloriaromanus/observer/Subject.java
@@ -0,0 +1,26 @@
+/**
+ * Subject interface of observer pattern
+ */
+
+package unsw.gloriaromanus.observer;
+
+public interface Subject {
+    /**
+     * Attach an observer to the subject's observer list
+     * 
+     * @param observer Observer object to attach
+     */
+    public void attach(Observer observer);
+
+    /**
+     * Detach an observer from the subject's observer list
+     * 
+     * @param observer Observer object to detach
+     */
+    public void detach(Observer observer);
+
+    /**
+     * Notify all of the subject's observers of an event
+     */
+    public void tell();
+}
