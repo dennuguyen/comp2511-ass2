7c92091028cf95da110d6aa48e39a9b37cba061b
dennuguyen <dennuguyen@gmail.com>
2020-10-25 23:12:01 +1100

    Initial commit

---
+++ src/unsw/gloriaromanus/Locable.java
@@ -0,0 +1,23 @@
+/**
+ * Locable interface
+ */
+
+package unsw.gloriaromanus;
+
+public interface Locable {
+
+    /**
+     * Gets the province location
+     * 
+     * @return Province location
+     */
+    public String getLocation();
+
+    /**
+     * Sets the province location
+     * 
+     * @param location New location
+     * @return Set location
+     */
+    public String setLocation(String location);
+}
---
+++ src/unsw/gloriaromanus/Locale.java
@@ -0,0 +1,25 @@
+/**
+ * Moveable interface
+ */
+
+package unsw.gloriaromanus;
+
+public class Locale implements Locable {
+
+    private String location;
+
+    public Locale(String spawn) {
+        location = new String(spawn);
+    }
+
+    @Override
+    public String getLocation() {
+        return this.location;
+    }
+
+    @Override
+    public String setLocation(String location) {
+        this.location = location;
+        return this.location;
+    }
+}
