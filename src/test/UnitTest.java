ec80f0b019aba1d9a43dfcf0c076d0f949bb3c26
Vi <vi.le@student.unsw.edu.au>
2020-10-27 00:47:25 +1100

    Test set tax rate

--- src/test/UnitTest.java
+++ src/test/UnitTest.java
@@ -6,18 +6,26 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import unsw.gloriaromanus.*;
+import unsw.gloriaromanus.Province;
+import unsw.gloriaromanus.component.*;
 
 public class UnitTest{
-    @Test
-    public void blahTest(){
-        assertEquals("a", "a");
-    }
+    // @Test
+    // public void blahTest(){
+    //     assertEquals("a", "a");
+    // }
     
+    // @Test
+    // public void blahTest2(){
+    //     Unit u = new Unit();
+    //     assertEquals(u.getNumTroops(), 50);
+    // }
+
     @Test
-    public void blahTest2(){
-        Unit u = new Unit();
-        assertEquals(u.getNumTroops(), 50);
+    public void testSetWealth() {
+        Province p = new Province("Britannia");
+        p.setTaxRate(Tax.lowTax);
+        assertEquals(p.getTaxRate(), 10);
     }
 }
 
