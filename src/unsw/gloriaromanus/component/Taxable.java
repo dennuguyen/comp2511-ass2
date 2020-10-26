bddd1ef474a1f4a5a986e6b95410097fe16fbd8d
dennuguyen <dennuguyen@gmail.com>
2020-10-26 18:11:58 +1100

    Add comments

--- src/unsw/gloriaromanus/component/Taxable.java
+++ src/unsw/gloriaromanus/component/Taxable.java
@@ -6,9 +6,22 @@ package unsw.gloriaromanus.component;
 
 public interface Taxable {
 
+    /**
+     * Gets the current tax rate
+     * 
+     * @return Tax rate
+     */
     public int getTaxRate();
 
+    /**
+     * Set the tax rate
+     * 
+     * @param rate New tax rate
+     */
     public void setTaxRate(int rate);
 
+    /**
+     * Collects the tax and notifies the province subject of tax returns
+     */
     public void collectTax();
 }
