a5a9812f1ca3a943aafa6f5f32a9f43c01585d45
Vi <vi.le@student.unsw.edu.au>
2020-10-26 17:28:10 +1100

    Add wealthable interface

---
+++ src/unsw/gloriaromanus/component/Wealthable.java
@@ -0,0 +1,32 @@
+/**
+ * Wealthable interface
+ */
+
+package unsw.gloriaromanus.component;
+
+public interface Wealthable {
+    
+    /**
+     * Returns the town wealth of a province
+     * @return town wealth
+     */
+    public int getWealth();
+
+    // /**
+    //  * T from town wealth
+    //  * @
+    //  */
+    // public int takeWealth(int amount);
+
+    /**
+     * Add amount to town wealth
+     * @param amount amount to add
+     */
+    public void addWealth(int amount);
+
+    /**
+     * Add to wealth growth 
+     */
+    public void addWealthGrowth(int rate);
+
+}
