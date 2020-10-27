1ee1124360b4cfb4b05be13c55ecdda0db6fd1fa
dennuguyen <dennuguyen@gmail.com>
2020-10-27 17:03:15 +1100

    Remove

--- src/unsw/gloriaromanus/component/Statable.java
+++ src/unsw/gloriaromanus/component/Statable.java
@@ -6,7 +6,19 @@ package unsw.gloriaromanus.component;
 
 public interface Statable {
 
+    /**
+     * Gets the stat specified by type
+     * 
+     * @param type Type of stat
+     * @return Value of stat
+     */
     public int getStat(Stats.Type type);
 
+    /**
+     * Set a stat to its new value
+     * 
+     * @param type  Type of stat
+     * @param value New Value of stat
+     */
     public void setStat(Stats.Type type, int value);
 }
