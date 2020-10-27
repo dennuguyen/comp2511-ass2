e6105c8103560a22d2634e884b492ccd269854df
Vi <vi.le@student.unsw.edu.au>
2020-10-28 03:55:32 +1100

    Add army class

--- src/unsw/gloriaromanus/Army.java
+++ src/unsw/gloriaromanus/Army.java
@@ -1,18 +1,32 @@
 package unsw.gloriaromanus;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import unsw.gloriaromanus.util.Component;
 
 public class Army implements Component {
 
-    ArrayList<Component>  units = new ArrayList<Component>();
+    ArrayList<Component>  children = new ArrayList<Component>();
+
+    public int getNumUnits() {
+        return children.size();
+    }
+
+    public Component getRandomUnit() {
+        Random rand = new Random();
+        int index = rand.nextInt(getNumUnits());
+        Component child = children.get(index);
+        if (child instanceof Army) 
+            ((Army)child).getRandomUnit();
+        return child;
+    }
 
     public void add(Component unit) {
-		units.add(unit);
+		children.add(unit);
 	}
 
 	public void remove(Component unit) {
-		units.remove(unit);
-	}
+		children.remove(unit);
+    }
 }
