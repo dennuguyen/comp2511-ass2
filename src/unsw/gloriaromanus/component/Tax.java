7ea2f0f642c392217d0ba5c68df37b7545c369f5
dennuguyen <dennuguyen@gmail.com>
2020-10-28 03:30:12 +1100

    Example of double observer pattern

--- src/unsw/gloriaromanus/component/Tax.java
+++ src/unsw/gloriaromanus/component/Tax.java
@@ -5,34 +5,32 @@
 package unsw.gloriaromanus.component;
 
 import java.util.ArrayList;
-
+import unsw.gloriaromanus.util.EventKind;
 import unsw.gloriaromanus.util.Observer;
 import unsw.gloriaromanus.util.Subject;
 
 public class Tax implements Taxable, Subject {
 
-    ArrayList<Observer> observers = new ArrayList<Observer>();
+    private ArrayList<Observer> wealthObservers;
+    private ArrayList<Observer> moraleObservers;
 
     TaxLevel taxLevel;
 
     public Tax() {
-        taxLevel = new LowTax();
+        this.wealthObservers = new ArrayList<Observer>();
+        this.moraleObservers = new ArrayList<Observer>();
+        this.taxLevel = new LowTax();
     }
 
     @Override
-    public float getTaxRate() {
-        return taxLevel.getTaxRate();
-    }
-
-    @Override
-    public int getWealthGrowthChange() {
-        return taxLevel.getWealthGrowthChange();
+    public int getTaxRate() {
+        return this.taxLevel.getTaxRate();
     }
 
     @Override
     public void setTaxLevel(TaxLevel taxLevel) {
         this.taxLevel = taxLevel;
-        tell();
+        tell(EventKind.EVENT_A);
     }
 
     public int collectTaxImple(int wealth) {
@@ -46,21 +44,32 @@ public class Tax implements Taxable, Subject {
     }
 
     @Override
-    public void attach(Observer observer) {
-        if (!this.observers.contains(observer)) {
-            this.observers.add(observer);
+    public void attach(Observer observer, EventKind eventKind) {
+        if (eventKind == EventKind.EVENT_A) {
+            if (!this.wealthObservers.contains(observer))
+                this.wealthObservers.add(observer);
+        } else if (eventKind == EventKind.EVENT_B) {
+            if (!this.moraleObservers.contains(observer))
+                this.moraleObservers.add(observer);
         }
     }
 
     @Override
-    public void detach(Observer observer) {
-        this.observers.remove(observer);
+    public void detach(Observer observer, EventKind eventKind) {
+        if (eventKind == EventKind.EVENT_A)
+            this.wealthObservers.remove(observer);
+        else if (eventKind == EventKind.EVENT_B)
+            this.moraleObservers.remove(observer);
     }
 
     @Override
-    public void tell() {
-        for (Observer observer : observers) {
-            observer.update(this);
+    public void tell(EventKind eventKind) {
+        if (eventKind == EventKind.EVENT_A) {
+            for (Observer observer : wealthObservers)
+                observer.update(this);
+        } else if (eventKind == EventKind.EVENT_B) {
+            for (Observer observer : moraleObservers)
+                observer.update(this);
         }
     }
 }
---
+++ src/unsw/gloriaromanus/util/EventKind.java
@@ -0,0 +1,5 @@
+package unsw.gloriaromanus.util;
+
+public enum EventKind {
+    EVENT_A, EVENT_B, EVENT_C;
+}
