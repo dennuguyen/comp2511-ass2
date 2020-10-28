9f3fd1f8c4381852894af74c5f26b0e0783a6579
dennuguyen <dennuguyen@gmail.com>
2020-10-28 20:09:29 +1100

    Initial commit

---
+++ src/test/WorldTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class WorldTest {
+
+    /**
+     * Check if provinces are correctly created
+     */
+    @Test
+    public void WorldCreationTest() {
+
+    }
+}
