1333a74417e44a2e58b6dc07f7ed30e5da47f701
dennuguyen <dennuguyen@gmail.com>
2020-10-29 22:45:07 +1100

    Change RomanLegionary stats

--- src/unsw/gloriaromanus/RomanLegionary.java
+++ src/unsw/gloriaromanus/RomanLegionary.java
@@ -13,6 +13,6 @@ public class RomanLegionary extends Unit {
      * Roman Legionary constructor
      */
     public RomanLegionary(String spawn) {
-        super(spawn, Move.Type.INFANTRY, new Stats(80, 90, 60, 50, 1000, 40, 10, 40));
+        super(spawn, Move.Type.INFANTRY, new Stats(4, 8, 10, 6, 50, 10, 1000, 5));
     }
 }
