69d6dca9000b68dba4a7f37e8a284f570ca07b76
Vi <vi.le@student.unsw.edu.au>
2020-10-31 04:11:08 +1100

    Add break class

---
+++ src/unsw/gloriaromanus/Break.java
@@ -0,0 +1,34 @@
+package unsw.gloriaromanus;
+
+import unsw.gloriaromanus.component.Stats;
+
+public class Break {
+
+  
+    /**
+     * Calculates chance of unit breaking after an engagement
+     * 
+     * @return chance of unit breaking
+     */
+    public int calculateBreakChance(Engagement e, Unit breaker, Unit enemy) {
+        int base = 100 - (breaker.getStat(Stats.Type.MORALE) * 10);
+                    
+        int x = e.calculateCasualties(e, breaker)/e.getInitialUnitSize(breaker);
+
+        int y = e.calculateCasualties(e, enemy)/e.getInitialUnitSize(enemy);
+
+        return base + x * 10 / y;
+    }
+
+    /**
+     * Determines if unit will break
+     * 
+     * @return if unit will break
+     */
+    public boolean isBroken(Engagement e, Unit breaker, Unit enemy) {
+        var d = Math.random() * 100;
+        if ((int) d < calculateBreakChance(e, breaker, enemy))
+            return true;
+        return false;
+    }
+}
