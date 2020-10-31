db772435272d3cfef42cfc0aab2aa207de261a29
dennuguyen <dennuguyen@gmail.com>
2020-10-31 18:51:18 +1100

    Implement shortest path function

--- src/test/MoveTest.java
+++ src/test/MoveTest.java
@@ -2,14 +2,23 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import unsw.gloriaromanus.World;
+import unsw.gloriaromanus.component.Move;
+
 public class MoveTest {
+
+    World world;
 
     @Before
     public void setUp() {
+        World.init("src/unsw/gloriaromanus/province_adjacency_matrix_fully_connected.json");
+        this.world = World.getInstance();
     }
 
     @After
@@ -17,6 +26,13 @@ public class MoveTest {
     }
 
     @Test
-    public void unitShouldBeAbleToMove() {
+    public void simpleShortestPathShouldReturnShortestPath() {
+        Move move = new Move(Move.Type.INFANTRY);
+        ArrayList<String> path = move.shortestPath("Britannia", "Lugdunensis");
+        assertEquals("[Britannia, Lugdunensis]", path.toString());
+    }
+
+    @Test
+    public void unitMovesShouldBeResetToCapacity() {
     }
 }
--- src/unsw/gloriaromanus/component/Move.java
+++ src/unsw/gloriaromanus/component/Move.java
@@ -4,11 +4,24 @@
 
 package unsw.gloriaromanus.component;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+import unsw.gloriaromanus.Province;
+import unsw.gloriaromanus.World;
 import unsw.gloriaromanus.util.Message;
 import unsw.gloriaromanus.util.PubSub;
 import unsw.gloriaromanus.util.PubSubable;
-import unsw.gloriaromanus.util.Topics;
 import unsw.gloriaromanus.util.Observer;
+import unsw.gloriaromanus.util.Pair;
 import unsw.gloriaromanus.util.Subject;
 
 public class Move implements Moveable, PubSubable, Observer {
@@ -54,8 +67,59 @@ public class Move implements Moveable, PubSubable, Observer {
         return exhaust - this.movesLeft;
     }
 
-    private int dijkstra() {
-        return 0;
+    public ArrayList<String> shortestPath(String start, String end) {
+        Map<String, Map<String, Boolean>> matrix = World.getInstance().getMap();
+
+        Map<String, Boolean> visited = new HashMap<String, Boolean>();
+        Map<String, String> breadcrumbs = new HashMap<String, String>();
+
+        for (String province : matrix.keySet()) {
+            visited.put(province, false);
+            breadcrumbs.put(province, null);
+        }
+
+        // Visit starting province
+        Queue<String> pqueue = new PriorityQueue<>();
+        Queue<String> nqueue = new PriorityQueue<>();
+        Queue<Integer> wqueue = new PriorityQueue<>();
+        visited.put(start, true);
+        pqueue.add(start);
+        nqueue.add(start);
+        wqueue.add(1);
+
+        while (!nqueue.isEmpty()) {
+
+            String pnode = pqueue.remove();
+            String nnode = nqueue.remove();
+            Integer wnode = wqueue.remove();
+
+            // Province not in breadcrumbs
+            if (breadcrumbs.get(nnode) == null) {
+
+                breadcrumbs.put(nnode, pnode);
+
+                // Found destination
+                if (nnode.equals(end)) {
+                    String[] path = new String[wnode];
+                    path[wnode - 1] = end;
+                    String b = end;
+                    for (int i = wnode - 2; i >= 0; i--)
+                        path[i] = b = breadcrumbs.get(b);
+                    return new ArrayList(Arrays.asList(path));
+                }
+
+                // Add connected children provinces
+                for (String p : matrix.get(nnode).keySet()) {
+                    if (matrix.get(nnode).get(p) == true) {
+                        pqueue.add(nnode);
+                        nqueue.add(p);
+                        wqueue.add(wnode + 1);
+                    }
+                }
+            }
+        }
+
+        return null;
     }
 
     @Override
