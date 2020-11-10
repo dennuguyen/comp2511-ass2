8aefd9f3c60a21c86e697de946f91545aa58f7f7
Vi Le <vi.le@student.unsw.edu.au>
2020-11-11 02:24:08 +1100

    Unstore faction and add getChildren

--- src/unsw/gloriaromanus/component/ConquestLeaf.java
+++ src/unsw/gloriaromanus/component/ConquestLeaf.java
@@ -1,28 +1,21 @@
 package unsw.gloriaromanus.component;
 
+import java.util.List;
+
 import unsw.gloriaromanus.Faction;
 import unsw.gloriaromanus.World;
 
 public class ConquestLeaf extends VictoryLeaf {
 
-    private World world;
-
-    /**
-     * Constructs conquest victory condition
-     * 
-     * @param f     player's faction
-     * @param world world in which game is set
-     */
-    public ConquestLeaf(Faction f, World world) {
-        super(f);
-        this.world = world;
-    }
-
     @Override
-    public Boolean evaluate() {
-        Faction f = super.getFaction();
-        if (f.numTerritories() == world.getMap().size())
+    public Boolean evaluate(Faction player, World world) {
+        if (player.numTerritories() == world.getMap().size())
             return true;
         return false;
+    }
+
+    @Override 
+    public List<VictoryCondition> getChildren() {
+        return null;
     }
 }
--- src/unsw/gloriaromanus/component/WealthLeaf.java
+++ src/unsw/gloriaromanus/component/WealthLeaf.java
@@ -1,23 +1,21 @@
 package unsw.gloriaromanus.component;
 
+import java.util.List;
+
 import unsw.gloriaromanus.Faction;
+import unsw.gloriaromanus.World;
 
 public class WealthLeaf extends VictoryLeaf {
-
-    /**
-     * Constructs wealth victory condition
-     * 
-     * @param f player's faction
-     */
-    public WealthLeaf(Faction f) {
-        super(f);
-    }
-
+    
     @Override
-    public Boolean evaluate() {
-        Faction f = super.getFaction();
-        if (f.calculateWealth() >= 400000)
+    public Boolean evaluate(Faction player, World world) {
+        if (player.calculateWealth() >= 400000)
             return true;
         return false;
+    }
+
+    @Override 
+    public List<VictoryCondition> getChildren() {
+        return null;
     }
 }
