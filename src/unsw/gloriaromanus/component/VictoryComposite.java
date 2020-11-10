119b5b9f4bf96f7dfb799b9794a16be2c9364f9b
Vi Le <vi.le@student.unsw.edu.au>
2020-11-11 02:25:35 +1100

    Add deserialize func

--- src/unsw/gloriaromanus/component/VictoryComposite.java
+++ src/unsw/gloriaromanus/component/VictoryComposite.java
@@ -3,6 +3,9 @@ package unsw.gloriaromanus.component;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import unsw.gloriaromanus.Faction;
 import unsw.gloriaromanus.World;
 
@@ -39,6 +42,26 @@ public abstract class VictoryComposite implements VictoryCondition {
     @Override 
     public List<VictoryCondition> getChildren() {
         return children;
+    }
+
+    public static VictoryCondition deserialize(JSONObject json) {
+        VictoryCondition vc = null;
+        switch (json.getString("type")) {
+            case "AND": vc = new AndComposite(); break;
+            case "OR": vc = new OrComposite(); break;
+        }
+        JSONArray array = json.getJSONArray("args");
+        for (int i = 0; i < array.length(); i++) {
+            JSONObject j = array.getJSONObject(i);
+            switch (j.getString("type")) {
+                case "AND": vc.add(VictoryComposite.deserialize(j)); break;
+                case "OR": vc.add(VictoryComposite.deserialize(j)); break;
+                case "TREASURY": vc.add(VictoryLeaf.deserialize(j)); break;
+                case "WEALTH": vc.add(VictoryLeaf.deserialize(j)); break;
+                case "CONQUEST": vc.add(VictoryLeaf.deserialize(j)); break;
+            }
+        }
+        return vc;
     }
 
 }
